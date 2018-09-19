0db1a7d0b1ef6c99f3605dfca2fc2c54d696844e
Freddymorato <Freddymorato@gmail.com>
2018-09-19 18:48:59 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+
+
+
+function produceDrivingRange(totalRange) {
+  return function(block1, block2) {
+    let range = parseInt(block2) - parseInt(block1)
+    if (range < totalRange) {
+      return `within range by ${totalRange - range}`
+      }
+      else {
+        return `${range - totalRange} blocks out of range`
+      }
+    }
+}
+
+function produceTipCalculator(percentage) {
+  return function(total) {
+    return total * percentage
+  }
+}
+
+function createDriver() {
+  let DriverId = 0;
+
+  return class {
+    constructor(name) {
+      this.name = name;
+      this.id = ++DriverId;
+    }
+  };
+}
